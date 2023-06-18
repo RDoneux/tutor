3c266de656eb72b9df588d35830235fb513387db
RDoneux <robertdoneux@gmail.com>
2023-06-18 12:53:16 +0100

    provides a not available route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { notAvailableInProductionGuard } from './guards/not-available-in-production.guard';
 
-const routes: Routes = [{ path: 'component-showroom', loadChildren: () => import('./pages/component-showroom/component-showroom.module').then(m => m.ComponentShowroomModule) }];
+const routes: Routes = [
+  {
+    path: 'component-showroom',
+    canActivate: [notAvailableInProductionGuard],
+    loadChildren: () =>
+      import('./pages/component-showroom/component-showroom.module').then(
+        (m) => m.ComponentShowroomModule
+      ),
+  },
+  {
+    path: 'not-available',
+    loadChildren: () =>
+      import('./pages/not-available/not-available.module').then(
+        (m) => m.NotAvailableModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/not-available/not-available.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotAvailableRoutingModule } from './not-available-routing.module';
+import { NotAvailableComponent } from './not-available.component';
+
+
+@NgModule({
+  declarations: [
+    NotAvailableComponent
+  ],
+  imports: [
+    CommonModule,
+    NotAvailableRoutingModule
+  ]
+})
+export class NotAvailableModule { }
