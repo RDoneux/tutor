9f7d2da0c8deb30c234001ea66f0da38e0c87256
RDoneux <robertdoneux@gmail.com>
2023-07-01 22:03:25 +0100

    fixes tests

--- src/app/pages/enquire/enquire.component.spec.ts
+++ src/app/pages/enquire/enquire.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnquireComponent } from './enquire.component';
+import { EnquiryFormModule } from 'src/app/modules/user-interface/enquiry-form/enquiry-form.module';
 
 describe('EnquireComponent', () => {
   let component: EnquireComponent;
@@ -8,7 +9,8 @@ describe('EnquireComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EnquireComponent]
+      declarations: [EnquireComponent],
+      imports: [EnquiryFormModule]
     });
     fixture = TestBed.createComponent(EnquireComponent);
     component = fixture.componentInstance;
