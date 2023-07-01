739c3c1210b9970571ef13e8bdcff2ce8b8a50b6
RDoneux <robertdoneux@gmail.com>
2023-07-01 12:46:55 +0100

    adds nav-bar e2e tests

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -15,6 +15,14 @@ describe('Navigation Bar', () => {
       cy.get('app-nav-bar').contains('Enquire').click();
       cy.url().should('include', 'enquire');
     });
+    it('should test home route', () => {
+      cy.visit('/pricing');
+      cy.get('.nav-bar__logo').click();
+      cy.url()
+        .should('not.include', 'about-me')
+        .should('not.include', 'pricing')
+        .should('not.include', 'enquire');
+    });
   });
 
   describe('Mobile Stylings', () => {
@@ -24,6 +32,9 @@ describe('Navigation Bar', () => {
     it('should not contain text navigation links in mobile view', () => {
       cy.get('.nav-bar__routes').should('have.css', 'display', 'none');
     });
+    it('should contain mobile icon in mobile view', () => {
+      cy.get('#menuIcon').should('have.css', 'display', 'inline-block');
+    });
 
     it('should display mobile navigation menu off screen', () => {
       cy.get('.nav-bar__mobile-menu').should(
@@ -31,6 +42,47 @@ describe('Navigation Bar', () => {
         'transform',
         'matrix(1, 0, 0, 1, -500, 0)'
       );
+    });
+
+    describe('Mobile Menu', () => {
+      beforeEach(() => {
+        cy.get('#menuIcon').click();
+      });
+
+      it('should display mobile navigation on screen once menu icon is clicked', () => {
+        cy.get('.nav-bar__mobile-menu').should(
+          'have.css',
+          'transform',
+          'matrix(1, 0, 0, 1, 0, 0)'
+        );
+      });
+      it('should display correct mobile menu items', () => {
+        cy.get('.nav-bar__mobile-menu').contains('Menu');
+        cy.get('.nav-bar__mobile-menu').contains('About Me');
+        cy.get('.nav-bar__mobile-menu').contains('Enquire');
+        cy.get('.nav-bar__mobile-menu').contains('Pricing');
+
+        cy.get('.nav-bar__mobile-menu').get('#closeIcon');
+
+        const facebookLogo = cy
+          .get('.nav-bar__mobile-menu')
+          .get('#facebookLogo');
+        facebookLogo.should('have.attr', 'target', '_blank');
+        const twitterLogo = cy.get('.nav-bar__mobile-menu').get('#twitterLogo');
+        twitterLogo.should('have.attr', 'target', '_blank');
+        const linkedInLogo = cy
+          .get('.nav-bar__mobile-menu')
+          .get('#linkedInLogo');
+        linkedInLogo.should('have.attr', 'target', '_blank');
+      });
+      it('should display mobile navigation off screen once close icon is clicked', () => {
+        cy.get('#closeIcon').click();
+        cy.get('.nav-bar__mobile-menu').should(
+          'have.css',
+          'transform',
+          'matrix(1, 0, 0, 1, -500, 0)'
+        );
+      });
     });
   });
 });
